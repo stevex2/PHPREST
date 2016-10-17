1728e63a77c6057500daa40f96b4b50710df307d
stevex2 <ennovathedons@gmail.com>
2016-10-17 03:28:00 +0300

    simple code to build on

---
+++ phprestwebservice.php
@@ -0,0 +1,37 @@
+<?php
+//process client request (via URL)
+include ("function.php");
+
+header("Content-Type:application/json");
+
+if (!empty($_GET['name'])) {
+
+	$name=$_GET['name'];
+	$price=get_price($name);
+
+	if(empty($price)){
+		//book not found
+		deliver_response(200,"book not found", NULL);
+	}
+	else{
+		//respond book price
+		deliver_response(200,"book found",$price);
+	}
+	
+}
+else{
+	//throw invalid request
+	deliver_response(400, "Invalid Requst",NULL);
+}
+
+function deliver_response($status,$status_message, $data){
+	 header("HTTP/1.1 $status $status_message $data");
+	 $response['status']=$status;
+	 $response['status_message']=$status_message;
+	 $response['data']=$data;
+	 $json_response=json_encode($response);
+	 echo $json_response;
+	
+}
+
+?>
